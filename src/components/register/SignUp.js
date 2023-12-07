c7d165a02b88f97be939b6b409103b696230ce07
gabutamashiro <gaby.tamashiro@gmail.com>
2023-12-06 23:39:23 -0300

    fix: sign up

--- src/components/register/SignUp.js
+++ src/components/register/SignUp.js
@@ -120,11 +120,9 @@ function SignUp(props) {
         <div className="signup__container">
           <div className="signup__title">Registrar-se</div>
           <div className="signup__close">
-            <img
-              alt="close"
-              onClick={() => toggleModal(false)}
-              src="https://static.xx.fbcdn.net/rsrc.php/v3/y2/r/__geKiQnSG-.png"
-            />
+             <svg onClick={() => toggleModal(false)} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" width={20} height={20}>
+              <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+            </svg>
           </div>
         </div>
         <div className="signup__subtitle"></div>
